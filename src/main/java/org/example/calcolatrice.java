d2be832dde469243891effeebbe84dcedee35d21
t.desimone5@studenti.unisa.it <tearosadesimone@gmail.com>
2023-09-26 14:45:56 +0200

    public void testmul(){         assertTrue(calc.multiply(2,1)==2,"somma corretta");     }     public void testdiv(){         assertTrue(calc.divide(2,1)==3,"somma corretta");     }

---
+++ src/main/java/org/example/calcolatrice.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class calcolatrice {
+
+    private int a;
+    private int b;
+
+    public calcolatrice(){
+
+        this.a =0;
+        this.b =0;
+    }
+    public int add (int a, int b){
+
+        return a+b;
+    }
+    public int sub (int a, int b){
+
+        return a-b;
+    }
+    public int multiply (int a, int b){
+
+        return a*b;
+    }
+    public int divide (int a, int b){
+
+        return a/b;
+    }
+
+
+
+}
---
+++ src/test/java/testcalcolatrice.java
@@ -0,0 +1,26 @@
+import org.example.calcolatrice;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class testcalcolatrice {
+
+    calcolatrice calc;
+
+    public final void  setup(){
+
+        calc= new calcolatrice();
+
+    }
+
+
+    public void testadd(){
+        assertTrue(calc.add(2,1)==3,"somma corretta");
+    }
+
+    public void testasub(){
+        assertTrue(calc.sub(2,1)==1,"sottrazione corretta");
+    }
+
+
+
+}
